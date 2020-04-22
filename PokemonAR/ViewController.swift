a3b75fefd8d5d22d2794ecd31d2bf32b7db5f1d0
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-22 00:08:26 -0700

    iOS

--- PokemonAR/ViewController.swift
+++ PokemonAR/ViewController.swift
@@ -55,27 +55,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
+  
+  func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    let node = SCNNode()
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    if let imageAnchor = anchor as? ARImageAnchor{
+      // create a plane using the image
+      let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+      
+      plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.8)
+      
+      
+      let planeNode = SCNNode(geometry: plane)
+      
+      planeNode.eulerAngles.x = -.pi/2
+      
+      node.addChildNode(planeNode)
+      
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+    
+    return node
+  }
+  
+  
+
 }
