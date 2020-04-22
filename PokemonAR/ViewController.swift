687c61d185b78645a3eb70558f0a96ef6abecfe1
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-21 19:37:24 -0700

    iOS

--- PokemonAR/ViewController.swift
+++ PokemonAR/ViewController.swift
@@ -26,18 +26,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
