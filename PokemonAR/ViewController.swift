6aaf126b39b418bc602296a257dc4f0af2ca30d3
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-22 01:36:22 -0700

    iOS

--- PokemonAR/ViewController.swift
+++ PokemonAR/ViewController.swift
@@ -14,52 +14,57 @@ import ARKit
 
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+  
+  @IBOutlet var sceneView: ARSCNView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        sceneView.autoenablesDefaultLighting = true
+    // Set the view's delegate
+    sceneView.delegate = self
+    
+    // Show statistics such as fps and timing information
+    sceneView.showsStatistics = true
+    
+    sceneView.autoenablesDefaultLighting = true
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    // Create a session configuration
+    //      ARImageTrackingConfiguration for 1 image is less resource intensive
+    
+    let configuration = ARWorldTrackingConfiguration()
+    // Bundle - location of the current file
+    if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "PokemonCards", bundle: Bundle.main){
+      configuration.detectionImages = imageToTrack
+      configuration.maximumNumberOfTrackedImages = 2
+      print("Images added!!")
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARImageTrackingConfiguration()
-      // Bundle - location of the current file
-      if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "PokemonCards", bundle: Bundle.main){
-        configuration.trackingImages = imageToTrack
-        configuration.maximumNumberOfTrackedImages = 1
-        print("Images added!!")
-      }
-      
-      
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
+    // Run the view's session
+    sceneView.session.run(configuration)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
     
+    // Pause the view's session
+    sceneView.session.pause()
+  }
+  
+  // MARK: - ARSCNViewDelegate
+  
   
   func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
     let node = SCNNode()
     
     if let imageAnchor = anchor as? ARImageAnchor{
+      
+      //      print(imageAnchor.referenceImage.name)
+      
       // create a plane using the image
       let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
       
@@ -72,19 +77,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
       
       node.addChildNode(planeNode)
       
-      
-     if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn"){
-      
-      if let pokeNode = pokeScene.rootNode.childNodes.first{
-        
-        
-        pokeNode.eulerAngles.x = .pi/2
-
-        planeNode.addChildNode(pokeNode)
-        
+      // render eevee
+      if imageAnchor.referenceImage.name == "eevee-card"{
+        if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn"){
+          
+          if let pokeNode = pokeScene.rootNode.childNodes.first{
+            pokeNode.eulerAngles.x = .pi/2
+            planeNode.addChildNode(pokeNode)
+          }
+        }
       }
       
+      // render oddish
+      if imageAnchor.referenceImage.name == "oddish-card"{
+        if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn"){
+          
+          if let pokeNode = pokeScene.rootNode.childNodes.first{
+            pokeNode.eulerAngles.x = .pi
+            planeNode.addChildNode(pokeNode)
+          }
+        }
       }
+      
       
       
     }
@@ -95,5 +109,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   }
   
   
-
+  
 }
